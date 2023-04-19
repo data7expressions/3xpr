8d6ac4444125f096625d708d8899b12fd6d258b0
FlavioLionelRita <flaviolrital@hotmail.com>
2023-04-19 23:29:55 +0200

    v1.9.12

--- build/lib/infrastructure/builder.d.ts
+++
@@ -1,5 +0,0 @@
-import { IBuilder } from '../domain';
-import { IExpressions } from '../application';
-export declare class ExpressionsBuilder implements IBuilder<IExpressions> {
-    build(): IExpressions;
-}
